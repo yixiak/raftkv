cf7869fb678b102ac7a59102737c2513ad75ff98
Yixa <1257597350@qq.com>
2023-12-27 14:01:46 +0800

    wIp: add kvserver struct

--- kvserver/kvserver.go
+++ kvserver/kvserver.go
@@ -4,3 +4,37 @@
 // https://opensource.org/licenses/MIT
 
 package kvserver
+
+import (
+	"raftkv/kvnode"
+	"sync"
+)
+
+type KVserver struct {
+	mu        sync.Mutex
+	node      *kvnode.KVnode
+	me        int32
+	storage   map[string]int
+	applychan chan kvnode.ApplyMsg
+}
+
+func NewKVServer(me int, peers []int, addrs []string, persist string) *KVserver {
+
+	applych := make(chan kvnode.ApplyMsg)
+	inner := kvnode.NewKVnode(me, peers, addrs, applych)
+
+	kvServer := &KVserver{
+		me:        int32(me),
+		node:      inner,
+		applychan: applych,
+	}
+	return kvServer
+}
+
+func (server *KVserver) Connect() {
+	server.node.Connect()
+}
+
+func (server *KVserver) IsLeader() bool {
+	return server.node.IsLeader()
+}
